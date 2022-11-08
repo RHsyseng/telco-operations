e01de1bc4dde905397366f90b6031d818a5c1b80
Karim Boumedhel <1840387+karmab@users.noreply.github.com>
2022-11-09 00:15:14 +0100

    support ipv6 in billi ansible (#47)

--- billi/ansible/boot.py
+++ billi/ansible/boot.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
 from redfish import Redfish
+import sys
 import yaml
 
-with open('inventory') as f:
- data = yaml.safe_load(f)['all']['vars']
+inventory = sys.argv[1] if len(sys.argv) > 1 else 'inventory'
+with open(inventory) as f:
+    data = yaml.safe_load(f)['all']['vars']
 
 
 default_bmc_user = data.get('bmc_user', 'root')
@@ -13,9 +15,9 @@ default_bmc_model = data.get('bmc_model', 'dell')
 iso_url = data['iso_url']
 hosts = data['hosts']
 for host in hosts:
-    bmc_url =  host.get('bmc_url')
-    bmc_user =  host.get('bmc_user', default_bmc_user)
-    bmc_password =  host.get('bmc_password', default_bmc_password)
+    bmc_url = host.get('bmc_url')
+    bmc_user = host.get('bmc_user', default_bmc_user)
+    bmc_password = host.get('bmc_password', default_bmc_password)
     bmc_model = host.get('bmc_model', default_bmc_model)
     if bmc_url is not None:
         red = Redfish(bmc_url, bmc_user, bmc_password, model=bmc_model)
